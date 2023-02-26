c65233c62e2dd94cbdabb32255cf8e9a97e3c535
enokaj2463 <126020924+enokaj2463@users.noreply.github.com>
2023-02-27 12:05:11 +1300

    Add files via upload

--- 01_sandpit.py
+++ 01_sandpit.py
@@ -1,1 +1,11 @@
-print("hello world")
+# ask user for a number
+get_number = input("Choose a number:")
+
+# multiply the number by 5
+times_five = get_number * 5
+
+answer = "{} times five is equal to" \
+         "{}".format(get_number, times_five)
+
+# output the result
+print()
